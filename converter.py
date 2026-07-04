ce16037f3764e6a5e21e055800ec342a6ec843e1
ngocngankhai294-alt <ngocngankhai294@gmail.com>
2026-07-04 21:04:02 +0800

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,14 @@
+def km_to_miles(km):
+    return km * 0.621371
+
+
+def miles_to_km(miles):
+    return miles / 0.621371
+
+
+def c_to_f(c):
+    return (c * 9 / 5) + 32
+
+
+def f_to_c(f):
+    return (f - 32) * 5 / 9
